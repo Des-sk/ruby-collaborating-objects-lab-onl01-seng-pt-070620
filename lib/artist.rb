bbd3bc1b2425ee379570db16efa8122a7c94755c
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-03 22:21:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,27 +23,7 @@
     Song.all.select {|songs| songs.artist == self}
   end
   
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-    
+  
   
   end
  
