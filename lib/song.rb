cae2166424fa8d3cb4c6a6fe0f12b80c4228d1fa
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-03 23:23:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,8 @@ class Song
   
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song@name = song_info[0]
+    song = Song.new(song_info)
+    song = song_info
     song
   end
   
