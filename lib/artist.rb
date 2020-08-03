d166499459d8054ff65ee9387534448998ea8356
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-03 22:24:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,25 @@
     Song.all.select {|songs| songs.artist == self}
   end
   
+  def self.find_or_create_by_name(name)   #class method uses find/create class 
+                                          #methods to detect or create instances
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)     #class method detects instances from @@all class variable
+    self.all.detect { |artist| artist.name == name } || self.all.create { |artist| artist.name == Artist.new(name)
+  end
   
+
+  def self.create(name)   #class method creates & stores instances vs initializing
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
   
   end
  
