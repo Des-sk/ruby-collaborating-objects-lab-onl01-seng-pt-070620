ac95dbb34cac299cb71e0deda0d7da88bbd58afc
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-04 01:04:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,29 +22,8 @@ class Song
     new_song
   end
 
-def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
-  def save
-    @@all << self
-  end
-
-
 
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
   
   
 end
