9b79ec07d88aa1166aa77f3856be329f062470fa
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-04 01:49:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,11 @@
 class Song
-  attr_accessor :name, :artist, :file_name
+  attr_accessor :name, :artist
   
   @@all= [ ]
   
   def initialize(name)
-    @name = name
-    @artist = artist
-    @file_name = file_name
+    @name= name
+    save
   end
   
   def save
@@ -17,13 +16,16 @@ class Song
     @@all
   end
   
-   def self.new_by_filename(file_name)
-      self.name = file_name.split(" - ")[0]
-      self.artist.name = file_name.split(" - ")[1]
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
   
-  
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-  
-  
+end
